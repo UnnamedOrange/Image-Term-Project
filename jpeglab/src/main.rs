37e6d6f155ccff39761bd9572850ca4ba1d53bd6
UnnamedOrange <lycheng1215@sina.com>
2024-05-02 22:23:26 +0800

    Parse arguments

--- jpeglab/src/main.rs
+++ jpeglab/src/main.rs
@@ -1,2 +1,18 @@
+use std::path::Path;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(
+        help = "Input image file",
+        long_help = "Input image file. To compress an image, the extension must be bmp. To uncompress an image, the extension must be jpg."
+    )]
+    input: String,
+}
+
 fn main() {
+    let args = Args::parse();
+    let path = Path::new(&args.input);
 }
