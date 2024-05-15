5f7a926773e695720b3828b9e80b399044e7ff51
UnnamedOrange <lycheng1215@sina.com>
2024-05-15 10:28:58 +0800

    Add comments

--- jpeglab/src/jpeglab/mod.rs
+++ jpeglab/src/jpeglab/mod.rs
@@ -24,22 +24,29 @@ use encode_step6::encode_step6;
 use encode_step7::encode_step7;
 
 pub fn encode(image: &RgbImage) -> io::Result<()> {
+    // 第一步：输入 RGB 的图像，输出 YUV422 的图像。
     let yuv_image = encode_step1(image)?;
     show_step1(&yuv_image);
 
+    // 第二步：输入 YUV422 图像，输出所有 MCU。
     let mcu_collection = encode_step2(&yuv_image)?;
     show_step2(&mcu_collection);
 
+    // 第三步：离散余弦变换。
     let dct_mcu_collection = encode_step3(&mcu_collection)?;
     show_step3(&dct_mcu_collection);
 
+    // 第四步：量化。
     let quantized_mcu_collection = encode_step4(&dct_mcu_collection)?;
     show_step4(&quantized_mcu_collection);
 
+    // 第五步：Zigzag。
     let zigzag_mcu_collection = encode_step5(&quantized_mcu_collection)?;
     show_step5(&zigzag_mcu_collection);
 
+    // 第六步：编码。
     let jpeg_output_data = encode_step6(&zigzag_mcu_collection)?;
 
+    // 第七步：输出 JPEG 文件。
     encode_step7(&jpeg_output_data)
 }
